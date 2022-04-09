6b9408a17d0f68c2b7dea44eff3184de8a460180
Megan Gedde <hoglu070@d.umn.edu>
2022-04-09 14:27:47 -0500

    added css and js

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// function getApi() {
+//     var requestUrl = 'https://api.github.com/users?per_page=5';
+
+//     fetch(requestUrl)
+//       .then(function (response) {
+//         return response.json();
+//       })
+//       .then(function (data) {
+//         // Use the console to examine the response
+//         console.log(data);
+       
+//         for (var i = 0; i < 6; i++)
+//         var repoName = repos[i].owner.login + "/" + repos[i].name;
+  
+//         var userName = document.createElement('h3')
+//       userName.fdsjk;l = data.(i).login
+//         // append container to the dom
+//         userContainer.appendChild(userName);
+//         // TODO: Loop through the data and generate your HTML
+//       });
+//   }
+//   fetchButton.addEventListener('click', getApi);
