92689f358ef8bdcbcc9a5bf1ae64231c1f68d550
Megan Gedde <hoglu070@d.umn.edu>
2022-04-09 20:42:51 -0500

    added city current values

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,9 @@ var forecastEl = document.querySelector('.forecast')
 var submitBtn = document.querySelector('.submitBtn')
 var cityDate = document.querySelector('.city-title')
 
-var submitHandler = function() {
-    //get value from input 
-    var city = cityNameEl.value.trim();
 
+var submitHandler = function() {
+    var city = cityNameEl.value.trim();
     if (city) {
         getWeather(city);
         //store city in local storage and display in history list
@@ -18,12 +17,44 @@ var submitHandler = function() {
 
 var getWeather = function() {
     //Display city, date and icon
-    cityDate.textContent = cityNameEl.value
-    //get current weather for city entered
+    var city = cityNameEl.value.trim();
+    cityDate.textContent = cityNameEl.value + " (" + moment().format('M/D/YY') + ")"
+        var key = '00e53e9dd9243b9df5f5b2fc939789ec';
+        fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city.toLowerCase() + '&appid=' + key)  
+        .then(function(resp) { return resp.json() }) // Convert data to json
+        .then(function(data) {
+            console.log(data)
+            //temp
+            var temp = document.createElement("p")
+            temp.textContent = "";
+            temp.textContent = "Temperature: " + (Math.round((data.main.temp-273.15)*9/5+32)) + " degrees farenheit" ;
+            cityDisplayEl.appendChild(temp);
+            //wind
+            var wind = document.createElement("p")
+            wind.textContent = "Wind: " + data.wind.speed + " per hour" ;
+            cityDisplayEl.appendChild(wind);
+            //humidity
+            var humidity = document.createElement("p")
+            humidity.textContent = "Humidity: " + data.main.humidity + "%" ;
+            cityDisplayEl.appendChild(humidity);
+            //uv index
+            var uv = document.createElement("p")
+            uv.textContent = "UV Index: " + data.main.temp ;
+            cityDisplayEl.appendChild(uv);
+        })
+    }
+    // fetch(apiUrl).then(function(response) {
+    //   if (response.ok) {
+    //     response.json().then(function(data) {
+    //     displayRepos(data.items, language);
+    //     });
+    //   } else {
+    //     alert('Error: GitHub User Not Found');
+    //   }
 
     
 
-}
+
 
 var getForecast = function() {
     //get 5 day forecast
