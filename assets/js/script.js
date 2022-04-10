3c704f62d67831b1793ff97ea0b6d0b288a2530c
Megan Gedde <hoglu070@d.umn.edu>
2022-04-10 13:14:43 -0500

    get icons to show

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,20 +9,27 @@ var submitHandler = function() {
     var city = cityNameEl.value.trim();
     if (city) {
         getWeather(city);
+        getForecast(city)
         //store city in local storage and display in history list
     } else {
         alert("Please enter a city")
     }
 }
 
+
 var getWeather = function() {
     //Display city, date and icon
     var city = cityNameEl.value.trim();
-    cityDate.textContent = cityNameEl.value + " (" + moment().format('M/D/YY') + ")"
         var key = '00e53e9dd9243b9df5f5b2fc939789ec';
         fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city.toLowerCase() + '&appid=' + key)  
         .then(function(resp) { return resp.json() }) // Convert data to json
         .then(function(data) {
+            //Display city and date
+            cityDate.textContent = cityNameEl.value + " (" + moment().format('M/D/YY') + ")"
+            //get weather icon
+            var iconurl = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+            $('#wicon').attr('src', iconurl);
+
             console.log(data)
             //temp
             var temp = document.createElement("p")
@@ -41,8 +48,41 @@ var getWeather = function() {
             var uv = document.createElement("p")
             uv.textContent = "UV Index: " + data.main.temp ;
             cityDisplayEl.appendChild(uv);
+
         })
     }
+
+    var getForecast = function() {
+        //Display 5 day forecast
+        var city = cityNameEl.value.trim();
+            var key = '00e53e9dd9243b9df5f5b2fc939789ec';
+            fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city.toLowerCase() + '&appid=' + key)  
+            .then(function(resp) { return resp.json() }) // Convert data to json
+            .then(function(data) {
+                for (var i = 0; i < 6; i++)
+                var forecast = data[i].owner.login + "/" + repos[i].name;
+                cityDate.textContent = cityNameEl.value + " (" + moment().format('M/D/YY') + ")" + data.weather[0].icon
+                console.log(data)
+                //temp
+                var temp = document.createElement("p")
+                temp.textContent = "";
+                temp.textContent = "Temperature: " + (Math.round((data.main.temp-273.15)*9/5+32)) + " degrees farenheit" ;
+                cityDisplayEl.appendChild(temp);
+                //wind
+                var wind = document.createElement("p")
+                wind.textContent = "Wind: " + data.wind.speed + " per hour" ;
+                cityDisplayEl.appendChild(wind);
+                //humidity
+                var humidity = document.createElement("p")
+                humidity.textContent = "Humidity: " + data.main.humidity + "%" ;
+                cityDisplayEl.appendChild(humidity);
+                //uv index
+                var uv = document.createElement("p")
+                uv.textContent = "UV Index: " + data.main.temp ;
+                cityDisplayEl.appendChild(uv);
+    
+            })
+        }
     // fetch(apiUrl).then(function(response) {
     //   if (response.ok) {
     //     response.json().then(function(data) {
